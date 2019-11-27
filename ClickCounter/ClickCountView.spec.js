f861783b17d8181e2145ae124e82f1119395c63d
myeonghuSong <21300390@handong.edu>
2019-11-28 00:58:19 +0900

    Click Count View Module spec-1-2

--- ClickCounter/ClickCountView.spec.js
+++ ClickCounter/ClickCountView.spec.js
@@ -1,12 +1,25 @@
 describe('App.ClickCountView', ()=> {
-  describe('updateView()', ()=> {
-    let updateEl, clickCounter, view;
-    beforeEach(() => {
-      clickCounter = App.ClickCounter();
-      updateEl = document.createElement('span');
-      view = App.ClickCountView(clickCounter, updateEl);
-    });
+  
+  let updateEl, clickCounter, view;
+  beforeEach(() => {
+    clickCounter = App.ClickCounter();
+    updateEl = document.createElement('span');
+    view = App.ClickCountView(clickCounter, updateEl);
+  });
+  
+  it('clickCounter를 주입하지 않으면 에러를 던진다.', () => {
+    const clickCounter = null;
+    const updateEl = document.createElement('span');
 
+    const actual = () => App.ClickCountView(clickCounter, updateView);
+    expect(actual).toThrowError();
+  });
+
+  // it('updateEl를 주입하지 않으면 에러를 던진다.', () => {
+
+  // })
+
+  describe('updateView()', ()=> {
     it('ClickCounter의 getValue() 값을 출력한다', ()=> {
         const counterValue = clickCounter.getValue();
         view.updateView();
